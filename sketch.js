65da246018555be27255a58238831d9a72fd9a58
RaushanKumar930 <76901661+RaushanKumar930@users.noreply.github.com>
2021-05-21 12:18:56 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -49,7 +49,7 @@ function draw() {
  
   //write code to display text lastFed time here
   fill("black")
-  if(lastFed >= 12){
+  if(lastFed > 12){
     text("Last Feed : " + lastFed % 12 + " PM", 350, 30);
   }else if(lastFed == 0){
     text("Last Feed : 12 AM",350, 30);
